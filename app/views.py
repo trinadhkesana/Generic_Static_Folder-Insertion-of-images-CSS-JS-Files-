e914abbae46702c67b7cf509544f3ca8a849f5c0
trinadhkesana <kesanatrinadh@gmail.com>
2023-11-28 10:47:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def virat(request):
+    return render(request,'virat.html')
+def pspk(request):
+    return render(request,'pspk.html')
